de28f97ab11a90f7d7734796fb8920f6656494e5
lucasffarias95 <lucasffarias95@gmail.com>
2026-01-11 15:42:57 -0300

    Commit inicial da calculadora simples v1

---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, essa é uma calculadora simples");
+            int opcao = -1;
+            double numero1, numero2;
+
+            while (opcao != 0)
+            {
+                Console.WriteLine(" -- Escolha a operaçao -- ");
+                Console.WriteLine("-- Opcao 0 - Sair -- ");
+                Console.WriteLine("-- Opçao 1 - Somar --");
+                Console.WriteLine("-- Opçao 2 - Subtrair --");
+                Console.WriteLine("-- Opçao 3 - Multiplicar --");
+                Console.WriteLine("-- Opçao 4 - Dividir --");
+
+                opcao = int.Parse(Console.ReadLine());
+
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o primeiro número:");
+                        numero1 = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Digite o segundo número:");
+                        numero2 = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"Resultado: {numero1 + numero2}");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Digite o primeiro número:");
+                        numero1 = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Digite o segundo número:");
+                        numero2 = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"Resultado: {numero1 - numero2}");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Digite o primeiro número:");
+                        numero1 = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Digite o segundo número:");
+                        numero2 = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"Resultado: {numero1 * numero2}");
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Digite o primeiro número:");
+                        numero1 = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Digite o segundo número:");
+                        numero2 = double.Parse(Console.ReadLine());
+
+                        if (numero2 == 0)
+                        {
+                            Console.WriteLine("Nao é possivel dividir por zero");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Resultado: {numero1 / numero2}");
+                        }
+                        break;
+                    case 0:
+                        Console.Write("Encerrando a calculadora..");
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcao inválida");
+                        break;
+                }
+            }
+        }
+    }
+}
+
